e8e7a551488f6944e37a7191f3e81824e6b28b4d
MSI\moneo <jvr.moneo@gmail.com>
2020-10-24 13:34:24 +0200

    Añadir estilos y contenido

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 "use strict";
+function search() {
+    console.log('search...');
+}
+function addContent() {
+    console.log('addContent...');
+}
+function deleteNote(id) {
+    console.log('deleteNote...');
+}
---
+++ ts/index.ts
@@ -0,0 +1,11 @@
+function search(): void{
+    console.log('search...');
+}
+
+function addContent(): void{
+    console.log('addContent...');
+}
+
+function deleteNote(id:number): void{
+    console.log('deleteNote...');
+}
