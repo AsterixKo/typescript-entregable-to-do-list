24d3aed4eaaa42a60b475a5719c796d77ef8b183
MSI\moneo <jvr.moneo@gmail.com>
2020-10-25 12:48:39 +0100

    solucionar comportamiento erroneo al borrar

--- js/index.js
+++ js/index.js
@@ -19,12 +19,6 @@ class Note {
 }
 let todoList = [];
 let listFound = [];
-let note1 = new Note(uuidv4(), 'Permanece conectado a lo que más te importa gracias a una batería de larga duración y a un diseño ligero y fino con bisel con microborde. Pensado para mantener la productividad y estar entretenido en cualquier parte, el portátil HP 15S-FQ de 15,6" (39,6 cm) ofrece un rendimiento fiable y una amplia pantalla, que te permite retransmitir y navegar entre tareas con rapidez desde el alba hasta el ocaso.');
-let note2 = new Note(uuidv4(), 'Aprovecha al máximo tu tiempo de juego con el ratón G203 2nd Gen para gaming, con tecnología LIGHTSYNC, un sensor para gaming y un diseño clásico con 6 botones. Anima tu juego... y tu escritorio.');
-let note3 = new Note(uuidv4(), 'Auriculares inalámbricos Sony WH-1000XM3B con función Noise Cancelling digital que te permite escuchar sin distracciones, optimización personal de Noise Cancelling y optimización de la presión atmosférica, libertad inalámbrica con la tecnología Bluetooth y NFC, la escucha inteligente mediante control de sonido adaptativo ajusta el sonido ambiental a tu actividad, audio de alta calidad con DSEE HX, S-Master HX y LDAC, control táctil: cambia de pista, sube o baja el volumen y responde llamadas tocando o deslizando el dedo sobre el panel.');
-todoList.push(note1);
-todoList.push(note2);
-todoList.push(note3);
 function search() {
     console.log('search...');
     let inputValue = $('#id-search').val();
@@ -41,7 +35,10 @@ function search() {
                 }
             }
         }
-        showNotes(listFound);
+        showNotesSearch(listFound);
+    }
+    else {
+        showNotes(todoList);
     }
 }
 function addContent() {
@@ -53,6 +50,7 @@ function addContent() {
         todoList.push(newNote);
         showNotes(todoList);
         $('#add-content').val('');
+        $('#id-search').val('');
     }
 }
 function deleteNote(id) {
@@ -65,6 +63,16 @@ function deleteNote(id) {
     });
     showNotes(todoList);
 }
+function deleteNoteFromSearch(id) {
+    console.log('deleteNote...', id);
+    todoList = todoList.filter(function (obj) {
+        return obj.id !== id;
+    });
+    listFound = listFound.filter(function (obj) {
+        return obj.id !== id;
+    });
+    showNotesSearch(listFound);
+}
 function showNotes(list) {
     $('#id-todo-content').empty();
     let resultDiv = '';
@@ -72,6 +80,17 @@ function showNotes(list) {
         resultDiv += `<div id="${list[i].id}" class="todo-content-box">`;
         resultDiv += `<div class="todo-content-note">${list[i].description}</div>`;
         resultDiv += `<div class="todo-content-buttons"><i class="far fa-trash-alt" onclick="deleteNote('${list[i].id}')"></i></div>`;
+        resultDiv += `</div>`;
+    }
+    $('#id-todo-content').append(resultDiv);
+}
+function showNotesSearch(list) {
+    $('#id-todo-content').empty();
+    let resultDiv = '';
+    for (let i = 0; i < list.length; i++) {
+        resultDiv += `<div id="${list[i].id}" class="todo-content-box">`;
+        resultDiv += `<div class="todo-content-note">${list[i].description}</div>`;
+        resultDiv += `<div class="todo-content-buttons"><i class="far fa-trash-alt" onclick="deleteNoteFromSearch('${list[i].id}')"></i></div>`;
         resultDiv += `</div>`;
     }
     $('#id-todo-content').append(resultDiv);
--- ts/index.ts
+++ ts/index.ts
@@ -29,12 +29,12 @@ let todoList: Note[] = [];
 let listFound: Note[] = [];
 
 //para hacer pruebas de búsqueda
-let note1: Note = new Note(uuidv4(), 'Permanece conectado a lo que más te importa gracias a una batería de larga duración y a un diseño ligero y fino con bisel con microborde. Pensado para mantener la productividad y estar entretenido en cualquier parte, el portátil HP 15S-FQ de 15,6" (39,6 cm) ofrece un rendimiento fiable y una amplia pantalla, que te permite retransmitir y navegar entre tareas con rapidez desde el alba hasta el ocaso.');
-let note2: Note = new Note(uuidv4(), 'Aprovecha al máximo tu tiempo de juego con el ratón G203 2nd Gen para gaming, con tecnología LIGHTSYNC, un sensor para gaming y un diseño clásico con 6 botones. Anima tu juego... y tu escritorio.');
-let note3: Note = new Note(uuidv4(), 'Auriculares inalámbricos Sony WH-1000XM3B con función Noise Cancelling digital que te permite escuchar sin distracciones, optimización personal de Noise Cancelling y optimización de la presión atmosférica, libertad inalámbrica con la tecnología Bluetooth y NFC, la escucha inteligente mediante control de sonido adaptativo ajusta el sonido ambiental a tu actividad, audio de alta calidad con DSEE HX, S-Master HX y LDAC, control táctil: cambia de pista, sube o baja el volumen y responde llamadas tocando o deslizando el dedo sobre el panel.');
-todoList.push(note1);
-todoList.push(note2);
-todoList.push(note3);
+// let note1: Note = new Note(uuidv4(), 'Permanece conectado a lo que más te importa gracias a una batería de larga duración y a un diseño ligero y fino con bisel con microborde. Pensado para mantener la productividad y estar entretenido en cualquier parte, el portátil HP 15S-FQ de 15,6" (39,6 cm) ofrece un rendimiento fiable y una amplia pantalla, que te permite retransmitir y navegar entre tareas con rapidez desde el alba hasta el ocaso.');
+// let note2: Note = new Note(uuidv4(), 'Aprovecha al máximo tu tiempo de juego con el ratón G203 2nd Gen para gaming, con tecnología LIGHTSYNC, un sensor para gaming y un diseño clásico con 6 botones. Anima tu juego... y tu escritorio.');
+// let note3: Note = new Note(uuidv4(), 'Auriculares inalámbricos Sony WH-1000XM3B con función Noise Cancelling digital que te permite escuchar sin distracciones, optimización personal de Noise Cancelling y optimización de la presión atmosférica, libertad inalámbrica con la tecnología Bluetooth y NFC, la escucha inteligente mediante control de sonido adaptativo ajusta el sonido ambiental a tu actividad, audio de alta calidad con DSEE HX, S-Master HX y LDAC, control táctil: cambia de pista, sube o baja el volumen y responde llamadas tocando o deslizando el dedo sobre el panel.');
+// todoList.push(note1);
+// todoList.push(note2);
+// todoList.push(note3);
 
 
 function search(): void {
@@ -53,7 +53,9 @@ function search(): void {
                 }
             }
         }
-        showNotes(listFound);
+        showNotesSearch(listFound);
+    }else{
+        showNotes(todoList);
     }
 }
 
@@ -67,6 +69,7 @@ function addContent(): void {
         todoList.push(newNote);
         showNotes(todoList);
         $('#add-content').val('');
+        $('#id-search').val('');
     }
 }
 
@@ -80,6 +83,17 @@ function deleteNote(id: string): void {
     });
     // todoList.splice(id, 1);
     showNotes(todoList);
+}
+
+function deleteNoteFromSearch(id: string): void {
+    console.log('deleteNote...', id);
+    todoList = todoList.filter(function (obj) {
+        return obj.id !== id;
+    });
+    listFound = listFound.filter(function (obj) {
+        return obj.id !== id;
+    });
+    showNotesSearch(listFound);
 }
 
 function showNotes(list: Note[]): void {
@@ -104,6 +118,34 @@ function showNotes(list: Note[]): void {
         resultDiv += `<div id="${list[i].id}" class="todo-content-box">`;
         resultDiv += `<div class="todo-content-note">${list[i].description}</div>`;
         resultDiv += `<div class="todo-content-buttons"><i class="far fa-trash-alt" onclick="deleteNote('${list[i].id}')"></i></div>`;
+        resultDiv += `</div>`;
+    }
+
+    $('#id-todo-content').append(resultDiv);
+}
+
+function showNotesSearch(list: Note[]): void {
+    //     <div id="id-todo-content" class="todo-content">
+    //     <div class="todo-content-box">
+    //         <div class="todo-content-note">
+    //             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla amet esse vitae numquam assumenda
+    //             sequi modi. Quam suscipit voluptatum modi repudiandae sed consequatur. Provident harum omnis est,
+    //             distinctio eveniet reprehenderit.
+    //             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla amet esse vitae numquam assumenda
+    //             sequi modi. Quam suscipit voluptatum modi repudiandae sed consequatur. Provident harum omnis est,
+    //             distinctio eveniet reprehenderit.
+    //         </div>
+    //         <div class="todo-content-buttons">
+    //             <i class="far fa-trash-alt" onclick="deleteNote('0')"></i>
+    //         </div>
+    //     </div>
+    // </div>
+    $('#id-todo-content').empty();
+    let resultDiv = '';
+    for (let i = 0; i < list.length; i++) {
+        resultDiv += `<div id="${list[i].id}" class="todo-content-box">`;
+        resultDiv += `<div class="todo-content-note">${list[i].description}</div>`;
+        resultDiv += `<div class="todo-content-buttons"><i class="far fa-trash-alt" onclick="deleteNoteFromSearch('${list[i].id}')"></i></div>`;
         resultDiv += `</div>`;
     }
 
